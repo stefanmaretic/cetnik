542ed52982b14e1714ab8b0aa8ab2a979a04fb4c
Stefan Maretić <smaretic@protonmail.com>
2019-02-02 17:56:32 +0100

    Initialize

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['prettier', 'eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+This project is inspired by [Discord](https://discordapp.com/).
+
+Built with:
+
+- [React](https://reactjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [CSS Modules](https://github.com/css-modules/css-modules)
+- [create-react-app](https://github.com/facebook/create-react-app)
